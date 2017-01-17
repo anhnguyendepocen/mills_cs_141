f0d62906449659059378b9a146f71004f5b8cce4
mpancia <msp@themsp.org>
2017-01-16 22:04:55 -0800

    Website update

--- render.R
+++ render.R
@@ -7,3 +7,13 @@ render_site()
 
 #Creates a PDF of the syllabus
 rmarkdown::render("syllabus.Rmd", "pdf_document",output_file="./_site/syllabus.pdf")
+
+#Inserts a download link to the PDF syllabus
+conn<-file("./_site/syllabus.html")
+text <- readLines(conn)
+close(conn)
+insert_position <- which(stringr::str_detect(text, ".*Last Updated.*")) + 1
+d_link <- "<li><i class='fa fa-download' aria-hidden='true'></i><a href='syllabus.pdf'> Download Syllabus (pdf)</a></li>"
+text_new <- c(text[1:insert_position],d_link,text[(insert_position+1):length(text)])
+write(text_new, file = "./_site/syllabus.html")
+
