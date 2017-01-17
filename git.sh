74b28331db9d4d4b21e9301859f69665f23254f0
mpancia <msp@themsp.org>
2017-01-16 21:42:19 -0800

    Website update

--- git.sh
+++ git.sh
@@ -2,3 +2,4 @@ cd "/Users/mpancia/Dropbox (Personal)/Mills/Mills Website" && git add .
 git remote add origin ssh://git@github.com:mpancia/mills_cs_141.git
 cd "/Users/mpancia/Dropbox (Personal)/Mills/Mills Website" && git commit -m 'Website update'
 cd "/Users/mpancia/Dropbox (Personal)/Mills/Mills Website" && git push -u origin master
+cd "/Users/mpancia/Dropbox (Personal)/Mills/Mills Website" && ghp-import -p -n -m "$(date)" _site
