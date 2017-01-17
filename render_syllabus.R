203feb8d940813068d2e12089275b49dedf9c147
mpancia <msp@themsp.org>
2017-01-16 21:44:33 -0800

    Website update

--- render_syllabus.R
+++ render_syllabus.R
@@ -6,7 +6,7 @@ setwd("~/Dropbox (Personal)/Mills/Mills Website/")
 render_site()
 
 #Creates a PDF of the syllabus
-rmarkdown::render("syllabus.Rmd", "pdf_document",output_file="./syllabus.pdf")
+rmarkdown::render("syllabus.Rmd", "pdf_document",output_file="./_site/syllabus.pdf")
 
 #Shell Script to copy files and move update git
 system("bash git.sh")
