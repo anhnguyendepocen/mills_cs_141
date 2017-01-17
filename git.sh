436a92c6204bebf02f22f1d0e44efb7cb5831a5f
mpancia <msp@themsp.org>
2017-01-16 21:49:50 -0800

    Website update

--- git.sh
+++ git.sh
@@ -1,5 +1,5 @@
+cd "/Users/mpancia/Dropbox (Personal)/Mills/Mills Website" && R  .
 cd "/Users/mpancia/Dropbox (Personal)/Mills/Mills Website" && git add .
-git remote add origin ssh://git@github.com:mpancia/mills_cs_141.git
 cd "/Users/mpancia/Dropbox (Personal)/Mills/Mills Website" && git commit -m 'Website update'
 cd "/Users/mpancia/Dropbox (Personal)/Mills/Mills Website" && git push -u origin master
 cd "/Users/mpancia/Dropbox (Personal)/Mills/Mills Website" && ghp-import -p -n -m "$(date)" _site
--- render_syllabus.R
+++ render_syllabus.R
@@ -7,7 +7,3 @@ render_site()
 
 #Creates a PDF of the syllabus
 rmarkdown::render("syllabus.Rmd", "pdf_document",output_file="./_site/syllabus.pdf")
-
-#Shell Script to copy files and move update git
-system("bash git.sh")
-
