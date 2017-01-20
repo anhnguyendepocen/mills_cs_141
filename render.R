be1719015ac830cfb39424bf332a848dbadded8f
mpancia <msp@themsp.org>
2017-01-19 21:58:25 -0800

    Website update

--- render.R
+++ render.R
@@ -18,3 +18,8 @@ d_link <- "<li><i class='fa fa-download' aria-hidden='true'></i><a href='pdfs/sy
 text_new <- c(text[1:insert_position],d_link,text[(insert_position+1):length(text)])
 write(text_new, file = "./_site/syllabus.html")
 
+#Move resources to resources folder
+source_directory <- "./resources/"
+target_directory <- "./_site/resources/"
+dir.create(target_directory)
+system(paste("cp -r", source_directory, target_directory)) 
